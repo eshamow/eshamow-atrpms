15bdb69f8462f623560d961437e0223f4070e384
Eric Shamow <eric.shamow@gmail.com>
2013-01-28 01:42:31 -0800

    Fixed init.pp comments

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,18 +2,10 @@
 #
 # Installs and enables ATrpms repository
 #
-# === Parameters
-#
-# Document parameters here.
-#
-# [*sample_parameter*]
-#   Explanation of what this parameter affects and what it defaults to.
-#   e.g. "Specify one or more upstream ntp servers as an array."
-#
 # === Examples
 #
 # Sample Usage:
-#   include epel
+#   include atrpms
 #
 # === Authors
 #
